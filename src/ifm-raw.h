ea678a925a29dc7ba3affb35232fa1cbd78e1b58
g_hutchi <g_hutchi@localhost>
1998-05-20 16:01:04 +0100

    Initial revision

---
+++ src/ifm-raw.h
@@ -0,0 +1,21 @@
+/*
+ *  Ifm version 1.0, Copyright (C) 1997 G. Hutchings
+ *  Ifm comes with ABSOLUTELY NO WARRANTY.
+ *  This is free software, and you are welcome to redistribute it
+ *  under certain conditions; see the file COPYING for details.
+ */
+
+/* Raw output driver header */
+
+#ifndef IFM_RAW_H
+#define IFM_RAW_H
+
+extern itemfuncs raw_itemfuncs;
+extern taskfuncs raw_taskfuncs;
+
+extern void raw_item_start(char *title);
+extern void raw_item_entry(vhash *item);
+extern void raw_task_start(char *title);
+extern void raw_task_entry(vhash *task);
+
+#endif
