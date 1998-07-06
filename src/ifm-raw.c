b9341e9a0f5ab4bf989ddd92a384bb20c2a0e931
g_hutchi <g_hutchi@localhost>
1998-07-06 18:48:21 +0100

    Removed task inventory stuff.  Needs finishing properly.

--- src/ifm-raw.c
+++ src/ifm-raw.c
@@ -56,8 +56,8 @@ raw_item_entry(vhash *item)
 void
 raw_task_start(void)
 {
-    printf("%s\t%s\t%s\t%s\t%s\n",
-           "ROOM", "TASK", "NOTE", "SCORE", "INV");
+    printf("%s\t%s\t%s\t%s\n",
+           "ROOM", "TASK", "NOTE", "SCORE");
 }
 
 void
@@ -65,23 +65,16 @@ raw_task_entry(vhash *task)
 {
     vhash *room = vh_pget(task, "ROOM");
     int score = vh_iget(task, "SCORE");
-    vlist *inv = vh_pget(task, "INVENT");
     char *note = vh_sgetref(task, "NOTE");
     static vhash *last = NULL;
-    vlist *carried = vl_create();
     vscalar *elt;
 
-    vl_foreach(elt, inv)
-        vl_spush(carried, vh_sgetref(vs_pget(elt), "DESC"));
-
-    printf("%s\t%s\t%s\t%s\t%s\n",
+    printf("%s\t%s\t%s\t%s\n",
            (room == last || room == NULL ? "" : vh_sgetref(room, "DESC")),
            vh_sgetref(task, "DESC"),
            (note == NULL ? "" : note),
-           (score ? vh_sget(task, "SCORE") : ""),
-           vl_join(carried, ", "));
+           (score ? vh_sget(task, "SCORE") : ""));
 
-    vl_destroy(carried);
     if (room != NULL)
         last = room;
 }
