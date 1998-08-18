4f940acdd2cc61a94cbabc7b2e76a5edb6f760d5
g_hutchi <g_hutchi@localhost>
1998-08-18 11:01:39 +0100

    Added lots more features.

--- progs/scr2ifm.pl
+++ progs/scr2ifm.pl
@@ -5,14 +5,14 @@
 use Getopt::Std;
 
 # Compass direction command -> direction mapping.
-%cdirmap = ("N" => "n", "NORTH" => "n", "NE" => "ne", "NORTHEAST" => "ne",
-	    "E" => "e", "EAST" => "e", "SOUTHEAST" => "se", "SE" => "se",
-	    "SOUTH" => "s", "S" => "s", "SOUTHWEST" => "sw", "SW" => "sw",
-	    "WEST" => "w", "W" => "w", "NORTHWEST" => "nw", "NW" => "nw");
+%cdirmap = ("n" => "n", "north" => "n", "ne" => "ne", "northeast" => "ne",
+	    "e" => "e", "east" => "e", "southeast" => "se", "se" => "se",
+	    "south" => "s", "s" => "s", "southwest" => "sw", "sw" => "sw",
+	    "west" => "w", "w" => "w", "northwest" => "nw", "nw" => "nw");
 
 # Other direction command -> direction mapping.
-%odirmap = ("UP" => "up", "U" => "up", "DOWN" => "down", "D" => "down",
-	    "IN" => "in", "OUT" => "out");
+%odirmap = ("up" => "up", "u" => "up", "down" => "down", "d" => "down",
+	    "in" => "in", "out" => "out");
 
 # Reverse-direction mapping.
 %rdirmap = ("n" => "s", "s" => "n", "ne" => "sw", "sw" => "ne", "e" => "w",
@@ -25,6 +25,9 @@ use Getopt::Std;
 # List of transcript moves (hash entries).
 @moves = ();
 
+# Room tag -> move mapping.
+%movemap = ();
+
 # Room tag -> room data mapping.
 %roommap = ();
 
@@ -33,9 +36,6 @@ use Getopt::Std;
 
 # List of IFM rooms.
 @rooms = ();
-
-# List of IFM links.
-@links = ();
 
 # Default room name recognition parameters.
 $name_maxwords = 8;
@@ -47,16 +47,17 @@ $desc_minwords = 20;
 
 # Default command regexps.
 $cmd_prompt = '^>\s*';
-$cmd_look = '^L(OOK)?$';
-$cmd_end = '^UNSCRIPT$';
-$cmd_undo = '^UNDO$';
-$cmd_invalid = '^RE(START|STORE)$';
+$cmd_look = '^l(ook)?$';
+$cmd_undo = '^undo$';
+$cmd_ifm = '^(title|map|item|task)';
+$cmd_teleport = '^(restart|restore)$';
+$cmd_ignore = '^unscript$';
 
 ### Stage 1 -- parse arguments and read input.
 
 # Parse arguments.
 $0 =~ s-.*/--;
-&getopts('c:dhlo:t:vw') or die "Type `$0 -h' for help\n";
+&getopts('c:hilo:w') or die "Type `$0 -h' for help\n";
 &usage if $opt_h;
 
 # Read IFM command file if required.
@@ -86,61 +87,67 @@ if ($opt_o) {
     open(STDOUT, "> $opt_o") or die "Can't open $opt_o: $!\n";
 }
 
-# Skip input until first prompt.
-while (<>) {
-    last if /$cmd_prompt/o;
-}
+foreach $file (@ARGV) {
+    open(IN, $file) or die "Can't open $file: $!\n";
 
-# Read command/reply blocks.
-&verbose(1, "Reading transcript...");
-
-while (1) {
-    # Get command.
-    s/$cmd_prompt//o;
-    chop($cmd = uc $_);
-    $cmd =~ s/\s+$//;
-    $line = $.;
-
-    # Check for end of script.
-    last if $cmd =~ /$cmd_end/o;
-
-    # Read reply.
-    $reply = [];
-    while (<>) {
-	last if /$cmd_prompt/o;
-	chop;
-	push(@$reply, $_);
+    # Skip input until first prompt.
+    while (<IN>) {
+	last if /$cmd_prompt/io;
     }
 
-    if ($cmd =~ /$cmd_undo/o) {
-	# Undo -- pop previous move (if any).
-	pop(@moves) unless $undone++;
-    } else {
-	# Record move.
-	$move = {};
-	$move->{CMD} = $cmd;
-	$move->{REPLY} = $reply;
-	$move->{LINE} = $line;
-	push(@moves, $move);
-	undef $undone;
+    # Read command/reply blocks.
+    while (1) {
+	# Get command.
+	s/$cmd_prompt//io;
+	chop($cmd = $_);
+	$cmd =~ s/\s+$//;
+	$line = $.;
+
+	# Read reply.
+	$reply = [];
+	while (<IN>) {
+	    last if /$cmd_prompt/io;
+	    chop;
+	    push(@$reply, $_);
+	}
+
+	if ($cmd =~ /$cmd_undo/io) {
+	    # Undo -- pop previous move (if any).
+	    pop(@moves) unless $undone++;
+	} else {
+	    # Record move.
+	    $move = {};
+
+	    $move->{CMD} = $cmd;
+	    $move->{REPLY} = $reply;
+	    $move->{FILE} = $file;
+	    $move->{LINE} = $line;
+	    $move->{TELE}++ if $teleport;
+	    push(@moves, $move);
+
+	    undef $teleport;
+	    undef $undone;
+	}
+
+	last if eof(IN);
     }
 
-    last if eof;
+    close IN;
+    $teleport++;
 }
 
-### Stage 2 -- scan moves for those which mark or change room.
-
-&verbose(1, "Scanning for movement commands...");
+### Stage 2 -- scan moves for movement commands.
 
 foreach $move (@moves) {
     undef $roomflag;
     undef $descflag;
     undef $desc;
 
-    &error("invalid transcript command: %s", $move->{CMD})
-	if $move->{CMD} =~ /$cmd_invalid/o;
+    # Check for ignored command.
+    next if $move->{CMD} =~ /$cmd_ignore/io;
 
-    next if $move->{CMD} =~ /$cmd_ignore/o;
+    # Check for teleport command.
+    $move->{TELE}++ if $move->{CMD} =~ /$cmd_teleport/io;
 
     for (@{$move->{REPLY}}) {
 	$blank = /^\s*$/;
@@ -173,9 +180,9 @@ foreach $move (@moves) {
 
     # Store room info (if any).
     if ($roomflag) {
-	$move->{ROOM} = $name;
+	$move->{NAME} = $name;
 	$move->{DESC} = $desc if $desc;
-	$move->{LOOK} = 1 if $move->{CMD} =~ /$cmd_look/o;
+	$move->{LOOK} = 1 if $move->{CMD} =~ /$cmd_look/io;
 
 	&warning("room `%s' verbose description is missing", $name)
 	    if !$desc && !$roomwarn{$name}++;
@@ -184,20 +191,28 @@ foreach $move (@moves) {
 
 ### Stage 3 -- Build IFM room and link lists.
 
-&verbose(1, "Building rooms and links...");
-
 foreach $move (@moves) {
-    $name = $move->{ROOM};
+    $name = $move->{NAME};
     $desc = $move->{DESC};
     $line = $move->{LINE};
+    $cmd = $move->{CMD};
+
+    # Check for IFM command.
+    if ($cmd =~ /$cmd_ifm/io) {
+	&ifmcmd($here, $line, $cmd);
+	next;
+    }
 
     # Skip it if no room is listed.
     next unless $name;
 
+    # If we teleported, try to find where we are now.
+    $here = &findroom($name, $desc) if $move->{TELE};
+
     # If it's a LOOK command, or we don't know where we are yet,
     # set current room.
     if ($move->{LOOK} || !$here) {
-	$here = &newroom($line, $name, $desc) unless $here;
+	$here = &newroom($move, $name, $desc) unless $here;
 	next;
     }
 
@@ -206,9 +221,6 @@ foreach $move (@moves) {
 
     # If the new room looks like this one, do nothing.
     next if $there eq $here;
-
-    # Get the movement direction.
-    $cmd = $move->{CMD};
 
     if      ($cdirmap{$cmd}) {
 	# Standard compass direction.
@@ -228,68 +240,85 @@ foreach $move (@moves) {
 
     if (!$there) {
 	# Unvisited -- new room.
-	$here = &newroom($line, $name, $desc, $dir, $here, $go, $cmd);
+	$here = &newroom($move, $name, $desc, $dir, $here, $go, $cmd);
     } else {
 	# Visited before -- new link.
-	&newlink($line, $here, $there, $dir, $go, $cmd);
+	&newlink($move, $here, $there, $dir, $go, $cmd);
 	$here = $there;
     }
 }
 
-### Stage 4 -- Write IFM output.
-
-&verbose(1, "Writing output...");
-
-print "## IFM map created from a transcript by $0\n";
-
-print "\ntitle \"$opt_t\";\n" if $opt_t;
-
-print "\n## Rooms generated by transcript.\n";
-
-foreach $room (@rooms) {
-    $name = $room->{NAME};
-    $desc = $room->{DESC};
-    $line = $room->{LINE};
+$room = $rooms[0];
+if ($needmap && $room && !$room->{JOIN}) {
     $tag = $room->{TAG};
-    $dir = $room->{DIR};
-    $from = $room->{FROM};
-    $go = $room->{GO};
-    $cmd = $room->{CMD};
-
-    print "room \"$name\" tag $tag";
-
-    print " dir $dir" if $dir;
-    print " from $from" if $from;
-    print " go $go" if $go;
-    print " cmd \"$cmd\"" if $cmd;
-
-    print ";";
-    print " # Line $line" if $opt_l;
-    print "\n";
-
-    print $desc if $opt_d && $desc;
+    $move = $movemap{$tag};
+    $move->{MAP} = "Map section 1";
 }
 
-print "\n## Extra links generated by transcript.\n" if @links > 0;
+### Stage 4 -- Write IFM output.
 
-foreach $link (@links) {
-    $from = $link->{FROM};
-    $to = $link->{TO};
-    $tag = $link->{TAG};
-    $line = $link->{LINE};
-    $dir = $link->{DIR};
-    $go = $link->{GO};
-    $cmd = $link->{CMD};
+print "## IFM map created by $0\n";
 
-    print "link $from to $to";
-    print " tag $tag" if $tag;
-    print " dir $dir" if $dir;
-    print " go $go" if $go;
-    print " cmd \"$cmd\"" if $cmd;
+print "\ntitle \"$title\";\n" if $title;
 
-    print ";";
-    print " # Line $line" if $opt_l;
-    print "\n";
+if ($needmap) {
+    print "\n## Map sections.\n";
+
+    foreach $move (@moves) {
+	$map = $move->{MAP};
+	print "map \"$map\";\n" if $map;
+    }
+}
+
+print "\n## Commands generated by transcript.\n";
+
+foreach $move (@moves) {
+    $list = $move->{IFM};
+    next unless $list;
+
+    undef $desc;
+    foreach $obj (@$list) {
+	$type = $obj->{TYPE};
+	$file = $move->{FILE};
+	$line = $obj->{LINE};
+	$name = $obj->{NAME};
+	$tag = $obj->{TAG};
+	$dir = $obj->{DIR};
+	$from = $obj->{FROM};
+	$to = $obj->{TO};
+	$go = $obj->{GO};
+	$cmd = $obj->{CMD};
+	$join = $obj->{JOIN};
+	$attr = $obj->{ATTR};
+
+	if ($opt_i) {
+	    if ($type =~ /(room|link)/) {
+		print "\n" if $linecount++;
+	    } else {
+		print "  ";
+	    }
+	}
+
+	print "$type ";
+
+	if ($type eq "link" || $type eq "join") {
+	    print "$from to $to";
+	    print " dir $dir" if $dir;
+	} else {
+	    print "\"$name\"";
+	    print " dir $dir" if $dir && !$join;
+	    print " from $from" if $from && !$join;
+	}
+
+	print " go $go" if $go && !$join;
+	print " cmd \"$cmd\"" if $cmd && !$join;
+	print " $attr" if $attr;
+	print " tag $tag" if $tag;
+
+	print ";";
+	print " \# $file ($line)" if $opt_l;
+	print "\n";
+    }
 }
 
 if (@ifmcmds > 0) {
@@ -302,13 +331,15 @@ sub roomname {
     my $line = shift;
 
     # Quick check for invalid format.
-    return 0 if $line =~ /$name_invalid/o;
+    return 0 if $line =~ /$name_invalid/io;
 
     # Check word count.
     my @words = split(' ', $line);
     return 0 if @words > $name_maxwords;
 
     # Check uncapitalized words.
+    return 0 if $line =~ /^[ a-z]/;
+
     for (@words) {
 	return 0 if /^[a-z]/ && length() > $name_maxuncap;
     }
@@ -318,15 +349,17 @@ sub roomname {
 
 # Add a new room to the room list.
 sub newroom {
-    my ($line, $name, $desc, $dir, $from, $go, $cmd) = @_;
-    my $tag = &maketag($name);
+    my ($move, $name, $desc, $dir, $from, $go, $cmd) = @_;
+    my $tag = &roomtag($name);
 
     my $room = {};
     push(@rooms, $room);
+    push(@{$move->{IFM}}, $room);
+    $movemap{$tag} = $move;
 
+    $room->{TYPE} = "room";
     $room->{NAME} = $name;
     $room->{DESC} = $desc;
-    $room->{LINE} = $line;
     $room->{WORDS} = [ split(' ', $desc) ];
     $room->{TAG} = $tag;
 
@@ -334,6 +367,7 @@ sub newroom {
     $room->{FROM} = $from if $from;
     $room->{GO} = $go if $go;
     $room->{CMD} = $cmd if $cmd;
+    $room->{LINE} = $move->{LINE};
 
     $roommap{$tag} = $room;
 
@@ -348,7 +382,7 @@ sub newroom {
 
 # Add a new link to the link list if required.
 sub newlink {
-    my ($line, $from, $to, $dir, $go, $cmd) = @_;
+    my ($move, $from, $to, $dir, $go, $cmd) = @_;
     my $link;
 
     if ($link = $linkmap{$from}{$to}) {
@@ -366,20 +400,38 @@ sub newlink {
     } else {
 	# No link exists -- create new one.
 	$link = {};
-	push(@links, $link);
+	$move->{LINK} = $link;
+	push(@{$move->{IFM}}, $link);
 
+	$link->{TYPE} = "link";
 	$link->{FROM} = $from;
 	$link->{TO} = $to;
 	$link->{TAG} = $from . "_" . $to;
-	$link->{LINE} = $line;
 	$link->{DIR} = $dir if $dir;
 	$link->{GO} = $go if $go;
 	$link->{CMD} = $cmd if $cmd;
+	$link->{LINE} = $move->{LINE};
 
 	&moveroom($from, $dir);
 	$roommap{$from}{$dir} = $to;
 	$linkmap{$from}{$to} = $link;
     }
+}
+
+# Add a new join to the join list if required.
+sub newjoin {
+    my ($move, $from, $to, $go, $cmd) = @_;
+
+    $join = {};
+    push(@{$move->{IFM}}, $join);
+
+    $join->{TYPE} = "join";
+    $join->{FROM} = $from;
+    $join->{TO} = $to;
+    $join->{TAG} = $from . "_" . $to;
+    $join->{GO} = $go if $go;
+    $join->{CMD} = $cmd if $cmd;
+    $join->{LINE} = $move->{LINE};
 }
 
 # Find a room and return its tag given a name and description.
@@ -472,7 +524,7 @@ sub choosedir {
 }
 
 # Make a room tag from its name.
-sub maketag {
+sub roomtag {
     my $name = shift;
     my $prefix;
 
@@ -492,16 +544,75 @@ sub maketag {
     return $tag;
 }
 
-# Print a verbose message.
-sub verbose {
-    return unless $opt_v;
+# Parse IFM command.
+sub ifmcmd {
+    my ($roomtag, $line, $cmd) = @_;
+    my $move = $movemap{$roomtag};
+    my $room = $roommap{$roomtag};
 
-    my $level = shift;
-    my $fmt = shift;
+    $cmd =~ s/\s*;\s*$//;
 
-    print STDERR "   " x ($level - 1) if $level;
-    printf STDERR $fmt, @_;
-    print STDERR "\n";
+    if ($cmd =~ /^title\s+"(.+)"/) {
+	# Set title.
+	$title = $1;
+    } elsif ($cmd =~ /^map\s+"(.+)"/) {
+	# Start new map section in this room.
+	$move->{MAP} = $1;
+	my $dir = $room->{DIR};
+
+	if ($dir) {
+	    my $go = $room->{GO};
+	    my @list = split(' ', $dir);
+	    $dir = shift @list;
+	    $cmd = $room->{CMD};
+	    &newjoin($move, $room->{FROM}, $room->{TAG}, $dir, $go, $cmd);
+	}
+
+	$room->{JOIN}++;
+	$needmap++;
+    } elsif ($cmd =~ /^(\S+)\s+"(.+)"\s*(.*)/) {
+	# Define object.
+	my $obj = {};
+
+	$obj->{TYPE} = $1;
+	$obj->{NAME} = $2;
+	$obj->{ATTR} = $3;
+	$obj->{LINE} = $line;
+	push(@{$move->{IFM}}, $obj);
+
+	if ($cmd =~ /^item/) {
+	    $lastitem = $obj;
+	} else {
+	    $lasttask = $obj;
+	}
+
+	if ($obj->{ATTR} !~ /\btag\b/) {
+	    my $tag = $obj->{NAME};
+	    $tag =~ s/[^A-Za-z0-9]/_/g;
+	    $obj->{ATTR} .= " " if $obj->{ATTR};
+	    $obj->{ATTR} .= "tag $tag";
+	}
+    } elsif ($cmd =~ /^(\S+)\s*(.*)/) {
+	# Add object attributes.
+	my $type = $1;
+	my $attr = $2;
+
+	if      ($type eq "item" && $lastitem) {
+	    $lastitem->{ATTR} .= " " if $lastitem->{ATTR};
+	    $lastitem->{ATTR} .= $attr;
+	} elsif ($type eq "task" && $lasttask) {
+	    $lasttask->{ATTR} .= " " if $lasttask->{ATTR};
+	    $lasttask->{ATTR} .= $attr;
+	}
+
+	if ($attr eq "delete") {
+	    if ($type eq "item") {
+		$lastitem->{DEL} = 1 if $lastitem;
+	    } else {
+		$lasttask->{DEL} = 1 if $lasttask;
+	    }
+	}
+    }
 }
 
 # Print a warning.
@@ -525,14 +636,13 @@ sub error {
 
 # Print a usage message and exit.
 sub usage {
-    print "Usage: $0 [options] [file]\n";
-    print "   -t title       add a title\n";
+    print "Usage: $0 [options] file [file...]\n";
     print "   -c file        add ifm commands from file\n";
     print "   -o file        write to given file\n";
+    print "   -i             indent and space output\n";
     print "   -l             print line number comments\n";
-    print "   -d             print room description comments\n";
-    print "   -v             be verbose about things\n";
     print "   -w             don't print warnings\n";
+    print "   -h             this help message\n";
 
     exit 0;
 }
