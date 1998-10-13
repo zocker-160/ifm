9af8ea56822ade1309c07e957565e35a40d4591b
g_hutchi <g_hutchi@localhost>
1998-10-13 15:35:11 +0100

    Initial revision

---
+++ acconfig.h
@@ -0,0 +1,19 @@
+
+/*
+ *  Ifm, copyright (C) 1997, 1998 G. Hutchings
+ *  Ifm comes with ABSOLUTELY NO WARRANTY.
+ *  This is free software, and you are welcome to redistribute it
+ *  under certain conditions; see the file COPYING for details.
+ */
+
+/* Configuration stuff */
+@TOP@
+
+/* Name of this package */
+#undef PACKAGE
+
+/* Package version */
+#undef VERSION
+
+/* Whether debugging malloc is required */
+#undef WITH_DMALLOC
