5296f471d72092c9df49f1e11b7aac9c62004745
Glenn Hutchings <zondo42@googlemail.com>
2004-10-18 08:06:15 +0100

    Bump year to 2004.

--- etc/header.tcl
+++ etc/header.tcl
@@ -1,5 +1,5 @@
 # This file is part of IFM, the Interactive Fiction Mapper,
-# copyright (C) G. Hutchings 1997-2002.
+# copyright (C) G. Hutchings 1997-2004.
 #
 # IFM comes with ABSOLUTELY NO WARRANTY.  This is free
 # software, and you are welcome to redistribute it under
